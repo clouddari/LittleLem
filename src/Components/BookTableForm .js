56a25f12efe2b7b31ccbe12388801299a289c871
Dariia Khmara <166187273+clouddari@users.noreply.github.com>
2024-12-03 13:27:49 -0500

    Added form validation

--- src/Components/BookTableForm .js
+++ src/Components/BookTableForm .js
@@ -3,6 +3,7 @@ import "../App.css";
 import { fetchAPI, submitAPI } from './api';
 import { useNavigate } from 'react-router';
 
+
 const BookTableForm = () => {
 
   const navigate = useNavigate();
@@ -16,24 +17,36 @@ const BookTableForm = () => {
     occasion: '',
   });
 
+  const [validation, setValidation] = useState({
+    name: true,
+    phone: true,
+    email: true,
+    date: true,
+  });
+
   const [availableTimes, setAvailableTimes] = useState([
     "17:00", "18:00", "19:00", "20:00", "21:00", "22:00"
   ]);
 
   const [bookedTimes, setBookedTimes]=useState([]);
 
+  const today = new Date();
+  today.setDate(today.getDate());
+  const minDate = today.toISOString().split('T')[0];  // Format as YYYY-MM-DD
+
+
   const handleChange = (e) => {
     const { name, value } = e.target;
-    setFormData({ ...formData, [name]: value });
+    const cleanedValue = name === "phone" ? value.replace(/\s+/g, '') : value;
+    setFormData({ ...formData, [name]: cleanedValue });
+    setValidation((prevValidation) => ({
+      ...prevValidation,
+      [name]: validateField(name, value),
+    }));
   };
-  const today = new Date();
-  today.setDate(today.getDate()-1);
-  const minDate = today.toISOString().split('T')[0];  // Format as YYYY-MM-DD
-  
   const handleDateChange = async (e) => {
     const date = e.target.value;
     if (new Date(date) < today ) {
-      alert("Date cannot be in the past");
       return;
     }
     setFormData({ ...formData, date });
@@ -51,12 +64,9 @@ const BookTableForm = () => {
   // Handle form submission
   const handleSubmit = async (e) => {
     e.preventDefault();
-
     try{
     const success = await submitAPI(formData);
     if (success) {
-      alert("Booking successful!");
-
       // Update booked times (persist to a server or local state)
       const updatedBookedTimes = [...bookedTimes, { date: formData.date, time: formData.time }];
       setBookedTimes(updatedBookedTimes); // Update state if local
@@ -69,54 +79,104 @@ const BookTableForm = () => {
   }
 };
 
+const validateField = (name, value) => {
+  if (name === "phone") return /[0-9]{10}/.test(value);
+  if (name === "email") return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(value);
+  if (name === "name") return value.trim().length > 0; // Basic non-empty validation
+  if (name === "date") return new Date(value);
+  if (name === "guests") return value >= 1 && value <= 20; // Validate guest count
+  return true; // For other fields
+};
+
+const isFormValid = Object.values(validation).every(isValid => isValid) && Object.values(formData).every(value => value.trim() !== '');
 
   return (
     <form className='form' onSubmit={handleSubmit}>
       <label>Name:</label>
-      <input type="text" name="name" value={formData.name} onChange={handleChange} required />
+      <input
+       type="text"
+       name="name"
+       value={formData.name}
+       onChange={handleChange}
+       className={validation.name === false ? "invalid" : "valid"}
+       required
+/>
+       {validation.name === false && <small className="error">Name is required</small>}
+
 
       <label>Phone Number:</label>
+
       <input
       type="tel"
       name="phone"
       value={formData.phone}
       onChange={handleChange}
-      required
-      pattern='[0-9]{10}0'
-      title="Phone must be 10 digits" />
+      className={validation.phone === false ? "invalid" : "valid"}
+      pattern="[0-9]{10}"
+      required/>
+      {validation.phone === false && <small className="error">Phone is required. Format: 1234567890</small>}
+
 
       <label>Email:</label>
-      <input type="email" name="email" value={formData.email} onChange={handleChange} required />
+      <input
+       type="email"
+       name="email"
+       value={formData.email}
+       onChange={handleChange}
+       className={validation.email === false ? "invalid" : "valid"}
+       required />
+       {validation.email === false && <small className="error">Please type the correct email</small>}
 
       <label>Date:</label>
       <input
-        type="date"
-        name="date"
-        value={formData.date}
-        onChange={handleDateChange}
-        required
-        min={minDate} // Apply the min date here
+    type="date"
+    name="date"
+    value={formData.date}
+    onChange={handleChange}
+    className={validation.date ? "valid" : "invalid"}
+    min={minDate}
+    required
 />
-
       <label htmlFor="time">Time:</label>
-      <select id="time" name="time" value={formData.time} onChange={handleChange}  min ={minDate} required>
+      <select
+      id="time"
+      name="time"
+      value={formData.time}
+      onChange={handleChange}
+      className={validation.time === false ? "invalid" : "valid"}
+      min ={minDate}
+      required>
         <option value="">Select Time</option>
         {availableTimes.map((time) => (
           <option key={time} value={time}>{time}</option>
         ))}
       </select>
+      {validation.time === false && <small className="error">Please choose available time. </small>}
 
       <label>Number of Guests:</label>
-      <input type="number" name="guests" value={formData.guests} onChange={handleChange} min="1" max="20" required />
+      <input
+      className={validation.guests === false ? "invalid" : "valid"}
+      type="number"
+       name="guests"
+       value={formData.guests}
+       onChange={handleChange}
+       min="1" max="20" required />
+       {validation.guests === false && <small className="error">Allowed amount of guests is from 1 to 20.</small>}
 
       <label>Occasion:</label>
-      <select name="occasion" value={formData.occasion} onChange={handleChange} required>
+      <select
+      name="occasion"
+      value={formData.occasion}
+      onChange={handleChange}
+      className={validation.time === false ? "invalid" : "valid"}
+      required>
         <option value="">Select Occasion</option>
         <option value="Birthday">Birthday</option>
         <option value="Anniversary">Anniversary</option>
       </select>
+      {validation.occasion === false && <small className="error">Choose the occasion.</small>}
 
-      <button className="form-button" type="submit">Book Table</button>
+      <button className="form-button" type="submit" disabled={!isFormValid}>Submit</button>
     </form>
   );
 };
--- src/Components/BookTableForm.test.js
+++ src/Components/BookTableForm.test.js
@@ -1,31 +1,9 @@
+global.TextEncoder = require('util').TextEncoder;
+
 import React from 'react';
 import { render, screen, fireEvent  } from '@testing-library/react';
 import BookTableForm  from "./BookTableForm ";
 import { fetchAPI } from './api';
-
-
-jest.mock('./api'); // Mock fetchAPI function
-
-test('calls fetchAPI and updates available times on date change', async () => {
-  // Mock fetchAPI to return a non-empty array
-  fetchAPI.mockResolvedValueOnce(['17:00', '18:00', '19:00']);
-  
-  render(<BookTableForm />);
-  
-  // Trigger date change
-  const dateInput = screen.getByLabelText(/date/i);
-  fireEvent.change(dateInput, { target: { value: '2024-12-10' } });
-  
-  // Assert fetchAPI was called
-  expect(fetchAPI).toHaveBeenCalledWith('2024-12-10');
-  
-  // Assert available times are updated
-  const timeOptions = await screen.findAllByRole('option');
-  expect(timeOptions).toHaveLength(4); // Including the default "Select Time" option
-});
-
-
-
 
 test('renders the text on the button', () => {
   render(<BookTableForm />);
@@ -45,11 +23,29 @@ test("renders the default available times", () => {
  // Test for updating availableTimes
  test('selecting a time updates the time in formData', () => {
     render(<BookTableForm  />);
-    
     const dropdown = screen.getByRole('combobox', { name: /time/i });
     fireEvent.change(dropdown, { target: { value: '19:00' } });
-    
     expect(dropdown.value).toBe('19:00'); // Verifying that the dropdown value is updated
   });
+
+  jest.mock('./api'); // Mock fetchAPI function
+
+test('calls fetchAPI and updates available times on date change', async () => {
+  // Mock fetchAPI to return a non-empty array
+  fetchAPI.mockResolvedValueOnce(['17:00', '18:00', '19:00']);
+  
+  render(<BookTableForm />);
+  
+  // Trigger date change
+  const dateInput = screen.getByLabelText(/date/i);
+  fireEvent.change(dateInput, { target: { value: '2024-12-10' } });
+  
+  // Assert fetchAPI was called
+  expect(fetchAPI).toHaveBeenCalledWith('2024-12-10');
+  
+  // Assert available times are updated
+  const timeOptions = await screen.findAllByRole('option');
+  expect(timeOptions).toHaveLength(4); // Including the default "Select Time" option
+});
 
 
