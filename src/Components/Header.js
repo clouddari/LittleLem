895fbb39f3aef22402bc1b9e59a7fd1a4528e7d0
Dariia Khmara <166187273+clouddari@users.noreply.github.com>
2024-12-11 00:17:21 -0500

    Responsive Header and the menu section

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,17 +1,28 @@
 import companyLogo from "../Assets/Logo.svg";
 import { HashLink as Link } from 'react-router-hash-link';
+import "./Header.css";
+import { useState } from "react";
+
+
 
 function Header(){
+    const [menuOpen, setMenuOpen]=useState(false);
+    const toggleMenu = () => {
+        setMenuOpen(!menuOpen);
+    };
+
     return(
         <header className="header">
-            <img className="logo" src={companyLogo} alt="Little Lemon Logo" height="90px"/>
             <nav className="navMenu">
-            <ul>
-                <li><a href="/">Home</a></li>
+            <img className="logo" src={companyLogo} alt="Little Lemon Logo" height="90px"/>
+            <button class="menu-toggle" onClick={toggleMenu}>☰</button>
+
+            <ul className={menuOpen ? "open": ""}>
+                <li><Link to="/">Home</Link></li>
                 <li><Link to="/#about-us">About</Link></li>
-                <li><a href="/Menu">Menu</a></li>
-                <li><a href="/booking">Reservations</a></li>
-                <li><a href="/LogIn">Login</a></li>
+                <li><Link to="/Menu">Menu</Link></li>
+                <li><Link to="/booking">Reservations</Link></li>
+                <li><Link to="/LogIn">Login</Link></li>
             </ul>
             </nav>
         </header>
